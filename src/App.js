dc069b92302ee071ad06b64137b9eb37fe9c0bb9
cyberslot <cyberslot@gmail.com>
2024-02-08 14:53:02 +0200

    Changed reqs & lims

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload in real time.
+          Edit <code>src/App.js</code> and save to reload immediately.
         </p>
         <a
           className="App-link"
