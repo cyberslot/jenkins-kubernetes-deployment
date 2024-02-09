bada05da1d0cc99d728c9ee31cbf5bbbbefc7125
cyberslot <cyberslot@gmail.com>
2024-02-09 11:04:41 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and to reload immediately.
+          Edit <code>src/App.js</code> and to reload instantly.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React NOW!
         </a>
       </header>
     </div>
